d98e538a2798762a3f8a7204fd94c61ebfa3f126
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2021-09-12 05:25:33 +0000

    Update formula

--- Formula/dockerhost.rb
+++ Formula/dockerhost.rb
@@ -7,7 +7,7 @@ class Dockerhost < Formula
   depends_on :docker
   depends_on :jq
 
-  version "1.0.0"
+  version "1.0.1"
   sha256 "8aad86aa7ca4b4fa599204fe51c7c3be23389c2b36a3d18df28b8377b5234228"
 
   url "https://raw.githubusercontent.com/leighmcculloch/dockerhost/v#{version}/dockerhost"
