865fc493d9ced11e2c12ade318babe50732c50b2
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2021-09-12 06:05:55 +0000

    Fix dependencies

--- Formula/dockerhost.rb
+++ Formula/dockerhost.rb
@@ -3,7 +3,7 @@ class Dockerhost < Formula
   homepage "https://github.com/leighmcculloch/dockerhost"
   bottle :unneeded
 
-  depends_on "multipass" => :cask
+  depends_on :multipass => :cask
   depends_on "docker"
   depends_on "jq"
 
