4d0f38d743c108f8db9dd126779f6dfefc54a6b2
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2021-09-12 06:18:25 +0000

    Change to cask

--- Formula/dockerhost.rb
+++ Formula/dockerhost.rb
@@ -1,22 +1,15 @@
-class Dockerhost < Formula
+cask "dockerhost" do
   desc "Setup a docker host on macOS in a lightweight Ubuntu vm."
   homepage "https://github.com/leighmcculloch/dockerhost"
-  bottle :unneeded
 
-  depends_on :multipass => :cask
-  depends_on "docker"
-  depends_on "jq"
+  depends_on cask: "multipass"
+  depends_on formula: "docker"
+  depends_on formula: "jq"
 
   version "1.0.5"
   sha256 "8aad86aa7ca4b4fa599204fe51c7c3be23389c2b36a3d18df28b8377b5234228"
 
   url "https://raw.githubusercontent.com/leighmcculloch/dockerhost/v#{version}/dockerhost"
 
-  def install
-    bin.install "dockerhost"
-  end
-
-  test do
-    system "#{bin}/dockerhost"
-  end
+  binary "dockerhost"
 end
